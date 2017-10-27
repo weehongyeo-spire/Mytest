2bd9bee33a119c0550dbe88d0f8117ac4cae4ddb
weehongyeo-spire <weehong.yeo@spire.com>
2017-10-27 11:57:02 +0800

    Extracted setup procedures into different documents

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+
+Test
+
+"""
+def print_stuff():
+   print ("hello 1\n")
+   print ("hello 2\n")
+
+
+   print ("hello 3\n")
+
+print("hello 4\n");
